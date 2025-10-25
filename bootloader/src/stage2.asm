5eedb454828a01de20bb0fb87889e32235e1c744
Milosz <s198114@student.pg.edu.pl>
2025-10-25 19:54:12 +0200

    ustawienie rejestrow segmentowych

--- bootloader/src/stage2.asm
+++ bootloader/src/stage2.asm
@@ -1,2 +1,16 @@
 BITS 16
-ORG 0x7C00
+ORG 0x7E00
+
+;wlaczenie A20
+;przejscie do protected mode
+    ;wylaczyc cli
+    ;ustawienie gtd
+    ;ustawienie bitu PE w rejestrze CR0
+;uruchomienie kernela - skok do tego adresu
+
+;set segment registers
+mov ds, 0
+mov es, 0
+mov fs, 0
+mov gs, 0
+mov ss, 0
